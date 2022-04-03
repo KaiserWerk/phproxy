7bd3f53449e370151faee6446a80d03b7c62ad25
Robin Kaiser <web@r-k.mx>
2022-04-03 17:03:39 +0200

    update HTTP handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/KaiserWerk/phproxy/internal/config"
 )
@@ -12,6 +13,16 @@ type Base struct {
 	Logger *log.Logger
 }
 
-func (b *Base) Handler(w http.ResponseWriter, r *http.Request) {
+func (b *Base) Handler(proxy *httputil.ReverseProxy, targetHost string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
 
+func (b *Base) CustomRouteHandler(proxy *httputil.ReverseProxy) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// TODO
+
+		proxy.ServeHTTP(w, r)
+	}
 }
